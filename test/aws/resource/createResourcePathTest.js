8055e892893c685e71d2dd3a7fe70b46b0adbe80
Luke van der Hoeven <hungerandthirst@gmail.com>
2016-01-21 18:04:51 -0500

    fix test

--- test/aws/resource/createResourcePathTest.js
+++ test/aws/resource/createResourcePathTest.js
@@ -1,9 +1,10 @@
 import sinon from 'sinon'
+import loadSelf from '../../../src/loadSelf'
 import createResourcePath from '../../../src/aws/resource/createResourcePath'
 
 describe('createResourcePath', () => {
   let spy;
-  let data = { definition: { baseUri: '/test/path/one/two/three' } };
+  let data = loadSelf({ definition: { baseUri: '/test/path/one/two/three' } });
 
   beforeEach(() => {
     spy = sinon.spy();
@@ -24,13 +25,13 @@ describe('createResourcePath', () => {
     parts.forEach((path, index) => {
       it(`creates a resource ${path}`, () => {
         expect(spy.getCall(index).args[0]).to.equal(path);
-      })
+      });
 
-      it(`chains ${path} to ${parts[index-1]}`, () => {
+      it(`chains ${path} onto ${parts[index-1]}`, () => {
         const current = spy.getCall(index);
         const before = spy.getCall(index-1);
 
-        if(before) { expect(current.calledBefore(before)).to.beTrue; }
+        if(before) { expect(current.calledAfter(before)).to.be.true; }
         else { expect(current).to.eql(spy.firstCall); }
       });
     });
