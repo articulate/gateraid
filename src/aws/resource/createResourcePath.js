e84e163c48ce2c97f0a7863d9104d2cfcc473256
Luke van der Hoeven <hungerandthirst@gmail.com>
2016-01-21 18:05:05 -0500

    refactor by the power of pipeP

--- src/aws/resource/createResourcePath.js
+++ src/aws/resource/createResourcePath.js
@@ -1,17 +1,14 @@
-import { reduce } from 'ramda'
+import { pipeP, map } from 'ramda'
 import urlParser from 'url'
 
 export default function createResourcePath(data) {
   const {
     definition: { baseUri },
-    lib: { createResource }
+    lib: { createResource },
   } = data;
 
   const { pathname } = urlParser.parse(baseUri);
   const parts = pathname.replace(/^\/|\/$/g, '').split('/');
 
-  return reduce(
-    (promise, part) => promise.then(createResource(part)),
-    Promise.resolve(data),
-    parts);
+  return pipeP(map(createResource, parts));
 }
