3380dcf9f0da1dae0e6218671681df200573212c
Luke van der Hoeven <hungerandthirst@gmail.com>
2016-01-21 17:34:14 -0500

    test and refactor createResourcePath

--- src/aws/resource/createResourcePath.js
+++ src/aws/resource/createResourcePath.js
@@ -1,17 +1,17 @@
+import { reduce } from 'ramda'
 import urlParser from 'url'
-import createResource from './createResource'
 
 export default function createResourcePath(data) {
-  const { definition: { baseUri } } = data;
+  const {
+    definition: { baseUri },
+    lib: { createResource }
+  } = data;
+
   const { pathname } = urlParser.parse(baseUri);
-
   const parts = pathname.replace(/^\/|\/$/g, '').split('/');
-  let promise = Promise.resolve(data);
 
-  // surely there's a better way to chain these
-  parts.forEach(part => {
-    promise = promise.then(createResource(part));
-  });
-
-  return promise;
+  return reduce(
+    (promise, part) => promise.then(createResource(part)),
+    Promise.resolve(data),
+    parts);
 }
--- test/aws/resource/createResourcePathTest.js
+++ test/aws/resource/createResourcePathTest.js
@@ -1,6 +1,39 @@
+import sinon from 'sinon'
 import createResourcePath from '../../../src/aws/resource/createResourcePath'
-import createResource from '../../../src/aws/resource/createResource'
 
 describe('createResourcePath', () => {
+  let spy;
+  let data = { definition: { baseUri: '/test/path/one/two/three' } };
+
+  beforeEach(() => {
+    spy = sinon.spy();
+    data.lib = { createResource: spy };
+
+    createResourcePath(data);
+  });
+
+  it("creates resources for each path segment", () => {
+    expect(spy.callCount).to.equal(5);
+  });
+
+  context('ordering', () => {
+    // split on / then drop the empty "" caused by the leading '/'
+    // the function does this internally but we need to mimic
+    const parts = data.definition.baseUri.split('/').slice(1);
+
+    parts.forEach((path, index) => {
+      it(`creates a resource ${path}`, () => {
+        expect(spy.getCall(index).args[0]).to.equal(path);
+      })
+
+      it(`chains ${path} to ${parts[index-1]}`, () => {
+        const current = spy.getCall(index);
+        const before = spy.getCall(index-1);
+
+        if(before) { expect(current.calledBefore(before)).to.beTrue; }
+        else { expect(current).to.eql(spy.firstCall); }
+      });
+    });
+  })
 
 });
